3db73699573e050f71052a57aa0eb1e5ac7883ef
Ahammed rufaid <58688964+rufaidem@users.noreply.github.com>
2021-12-10 10:58:12 +0530

    delected card

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,6 @@ import { StaticImage } from "gatsby-plugin-image"
 import { Helmet } from "react-helmet"
 import Particles from "react-particles-js"
 
-const cards = document.querySelectorAll(".box");
-
-cards.forEach((card) => {
-  card.addEventListener("mousemove", (e) => {
-    const rect = card.getBoundingClientRect();
-    const x = ((e.clientX - rect.left) / card.clientWidth) * 100;
-    const y = ((e.clientY - rect.top) / card.clientHeight) * 100;
-
-    card.style.background = `radial-gradient(circle closest-corner
-      at ${x}% ${y}%,
-      var(--ripple-color), var(--bg-color))`;
-  });
-
-  card.addEventListener("mouseleave", (event) => {
-    card.style.removeProperty("background");
-  });
-});
 
 
 const particlesParams = {
